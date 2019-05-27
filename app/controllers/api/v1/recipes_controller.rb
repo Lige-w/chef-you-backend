6c538f58f7fe2a77d9f1ab60d7ad841cc0db19a9
github username <github email address>
2019-05-27 16:36:08 -0400

    opened routes for recipes

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::RecipesController < ApplicationController
 
+  def index
+    render json: Recipe.all
+  end
+
   def create
-    byebug
+
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,9 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :instructions
   has_many :quantities
   has_many :ingredients, through: :quantities
+
+  accepts_nested_attributes_for :quantities
+  accepts_nested_attributes_for :instructions
 end
