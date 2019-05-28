1852daa0d4c7292bbbdb4d9997cbb4dce511bf71
“Lige-w” <Lige.williamson@gmail.com>
2019-05-28 11:40:21 -0400

    added ingredients to recipe serializer

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,5 +1,6 @@
 class RecipeSerializer < ActiveModel::Serializer
   has_many :instructions
   has_many :quantities
+  has_many :ingredients, through: :quantities
   attributes :id, :name, :description, :servings, :user_id
 end
