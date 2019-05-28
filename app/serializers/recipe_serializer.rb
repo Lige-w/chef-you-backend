baf78a76179f749cfcea6de77e7fdc6fd34ca09a
“Lige-w” <Lige.williamson@gmail.com>
2019-05-28 14:03:12 -0400

    delegated name to quantities

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,6 +1,5 @@
 class RecipeSerializer < ActiveModel::Serializer
   has_many :instructions
   has_many :quantities
-  has_many :ingredients, through: :quantities
   attributes :id, :name, :description, :servings, :user_id
 end
