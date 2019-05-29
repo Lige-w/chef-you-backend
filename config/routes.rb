c19f3766029e02231061fb7226b31e13b367d141
github username <github email address>
2019-05-29 10:01:11 -0400

    opened update route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :create]
-      resources :recipes, only: [:index, :create, :show]
+      resources :recipes, only: [:index, :create, :show, :update]
       resources :ingredients, only: [:index]
       post '/login', to: 'sessions#create'
     end
