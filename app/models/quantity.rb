058a0c0ed4deefc5eaa1e64e1207aed839024369
“Lige-w” <Lige.williamson@gmail.com>
2019-05-29 15:48:03 -0400

    not savung a new ingredient every time.

--- app/models/quantity.rb
+++ app/models/quantity.rb
@@ -5,4 +5,8 @@ class Quantity < ApplicationRecord
   accepts_nested_attributes_for :ingredient
 
   delegate :name, to: :ingredient
+
+  def ingredient_attributes=(ingredient)
+    self.ingredient = Ingredient.find_or_create_by(name: ingredient[:name])
+  end
 end
