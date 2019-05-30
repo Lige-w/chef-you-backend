b75e5e2233c033959e6f5ba68737aa164eb667ac
github username <github email address>
2019-05-29 22:24:43 -0400

    delete done

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     @user = User.find_by(username: params[:username])
-
     render json: @user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users, only: [:index, :create]
+      resources :users, only: [:index, :create, :show]
       resources :recipes, only: [:index, :create, :show, :update, :destroy]
       resources :ingredients, only: [:index]
       post '/login', to: 'sessions#create'
