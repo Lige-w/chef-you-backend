b06f2c4449d2593cfd2047361cba88c2f8823498
github username <github email address>
2019-05-29 20:36:36 -0400

    opened destroy route

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,8 +1,10 @@
 class Recipe < ApplicationRecord
   belongs_to :user
-  has_many :instructions
-  has_many :quantities
+  has_many :instructions, dependent: :destroy
+  has_many :quantities, dependent: :destroy
   has_many :ingredients, through: :quantities
+
+  validates :name, presence: true
 
   accepts_nested_attributes_for :quantities
   accepts_nested_attributes_for :instructions
