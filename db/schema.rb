becce83e9f3f14372b37837a5061aa828ca4ee85
github username <github email address>
2019-05-31 10:20:36 -0400

    instruction fields can be inserted

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_31_002144) do
+ActiveRecord::Schema.define(version: 2019_05_31_140619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_05_31_002144) do
     t.bigint "recipe_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "index"
     t.index ["recipe_id"], name: "index_instructions_on_recipe_id"
   end
 
@@ -36,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_05_31_002144) do
     t.string "unit"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "index"
     t.index ["ingredient_id"], name: "index_quantities_on_ingredient_id"
     t.index ["recipe_id"], name: "index_quantities_on_recipe_id"
   end
