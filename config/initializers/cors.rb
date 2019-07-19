5754cc9d27b743c1eab3f8125a39398f576583be
github username <github email address>
2019-07-19 11:33:03 -0400

    change cors to heroku url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins ['http://chef-you.herokuapp.com', 'https://chef-you.herokuapp.com']
 
     resource '*',
       headers: :any,
