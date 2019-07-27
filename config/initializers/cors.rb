225796c71461e430b967944fce67eef3a606a402
github username <Lige.williamson@gmail.com>
2019-07-27 14:48:58 -0400

    comment out default cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins ['http://chef-you.herokuapp.com', 'https://chef-you.herokuapp.com']
+    # origins '*'
 
     resource '*',
       headers: :any,
